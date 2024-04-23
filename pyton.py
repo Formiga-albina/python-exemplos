58aa15d40f89507b0d071a278782b8c800fe8045
Formiga-aninha <anaclarasebastiao1716@gmail.com>
2024-04-23 16:41:26 -0300

    Create variaveis.py

---
+++ pyton.py
@@ -0,0 +1,12 @@
+nome="Ana CLara"
+idade=15
+email="anaclara90@gmail.com"
+telefone="192"
+nota=9.43
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
