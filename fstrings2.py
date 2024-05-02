f6916ace072cb66ec7e8b4c4b6b492953d720759
Formiga-aninha <anaclarasebastiao1716@gmail.com>
2024-05-02 16:52:09 -0300

    Atualização fstrings2.py

--- fstrings2.py
+++ fstrings2.py
@@ -2,6 +2,10 @@
 escola =' Senai '
 curso = ' Desemvolvimento de Sistemas'
 uc = ' lógica de programacão '
+matricula = 34
+nota = 8.999999
 print(f"Escola : {escola}\n"
       f"Curso :{curso}\n"
-      f"Unidade Curricular: {uc}")
+      f"Unidade Curricular: {uc}\n"
+      f"Matricula:{matricula:06d}"
+      f"nota :{nota:.2f}")
