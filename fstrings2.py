a6b431203e540852b98ee9538368cd95b93f48e2
Formiga-aninha <anaclarasebastiao1716@gmail.com>
2024-05-02 16:32:59 -0300

    Create fstrings2.py

---
+++ fstrings2.py
@@ -0,0 +1,7 @@
+#formação de strings com várias linhas 
+escola =' Senai '
+curso = ' Desemvolvimento de Sistemas'
+uc = ' lógica de programacão '
+print(f"Escola : {escola}\n"
+      f"Curso :{curso}\n"
+      f"Unidade Curricular: {uc}")
